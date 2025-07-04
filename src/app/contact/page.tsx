bbeada004005fa5b8588f73588126129e746831e
hafsabibi195 <75561850+hafsabibi195@users.noreply.github.com>
2025-07-05 00:56:21 +0500

    fix: temporary contact page to pass build

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+
+export default function ContactPage() {
+  return (
+    <div className="flex items-center justify-center h-screen text-center">
+      <h1 className="text-3xl font-bold text-amber-800">Contact Page Coming Soon!</h1>
+    </div>
+  );
+}
