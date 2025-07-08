6c50a47202c977c67cb6a8e690db75f6ad9710f1
hafsabibi195 <75561850+hafsabibi195@users.noreply.github.com>
2025-07-08 23:49:04 +0500

    Make Hero section fully mobile responsive

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -2,7 +2,7 @@ export default function CheckoutPage() {
   return (
     <div className="flex items-center justify-center h-screen text-center">
       <h1 className="text-3xl font-bold text-amber-800">
-        Checkout Page Coming Soon!
+        Your order is complete!
       </h1>
     </div>
   );
